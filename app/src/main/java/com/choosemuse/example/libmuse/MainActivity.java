0a54d966e1f33e509d23abd7142f1eb2b0ebc707
Rae <raeleneg@uci.edu>
2016-10-22 14:34:12 -0700

    record class and average algorithm set up

--- app/src/main/java/com/choosemuse/example/libmuse/MainActivity.java
+++ app/src/main/java/com/choosemuse/example/libmuse/MainActivity.java
@@ -59,9 +59,6 @@ import android.widget.TextView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 
-//Clockwork API
-import com.clockworksms.*;
-
 /**
  * This example will illustrate how to connect to a Muse headband,
  * register for and receive EEG data and disconnect from the headband.
@@ -434,12 +431,11 @@ public class MainActivity extends Activity implements OnClickListener {
                 if (!averageSet) {
                     record.setMeanAccel(p.getAccelerometerValue(Accelerometer.X) + p.getAccelerometerValue(Accelerometer.Y) + p.getAccelerometerValue(Accelerometer.Z));
                 } else {
-                    //do checking here
-                }
-                if (record.isSeizure()){
-                    //send possible seizure alert
-                } else if (record.isPanicAttack()){
-                    //send possible panic attack alert
+                    if (record.isSeizure()) {
+                        //send possible seizure alert
+                    } else if (record.isPanicAttack()) {
+                        //send possible panic attack alert
+                    }
                 }
                 eegStale = true;
                 break;
@@ -813,31 +809,4 @@ public class MainActivity extends Activity implements OnClickListener {
             activityRef.get().receiveMuseArtifactPacket(p, muse);
         }
     }
-    
-    //Clockwork API
-    public class DemoSms
-{
-   public static void main(String[] args)
-   {
-      try
-      {
-         ClockWorkSmsService clockWorkSmsService = new ClockWorkSmsService("apikey");
-         SMS sms = new SMS("XXXXXXXXX", "Hello World");
-         ClockworkSmsResult result = clockWorkSmsService.send(sms);
-
-         if(result.isSuccess())
-         {
-            System.out.println("Sent with ID: " + result.getId());
-         }
-         else
-         {
-            System.out.println("Error: " + result.getErrorMessage());
-         }
-      }
-      catch (ClockworkException e)
-      {
-         e.printStackTrace();
-      }
-   }
-}
 }
